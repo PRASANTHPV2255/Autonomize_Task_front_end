5854f87691307f9baf561dace7770320cf9e93a6
Prasanth <prasanthpv30@gmail.com>
2024-07-23 16:48:56 +0530

    input area ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 
 import Header from './components/header'
+import TextArea from './components/TextArea'
 
 function App() {
 
   return (
     <div >
       <Header />
+      <TextArea/>
     </div>
   )
 }
--- src/components/TextArea/index.jsx
+++ src/components/TextArea/index.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import './style.css'
 
 function Index() {
   return (
-    <div>Index</div>
+    <div className='text_area'>
+      <div className='text_area_headings'>
+        <h2>GitHub Explorer</h2>
+        <p>Search username here</p>
+      </div>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder='Search'
+          color='black'
+        />
+        <button type="submit">Search</button>
+      </div>
+    </div>
   )
 }
 
