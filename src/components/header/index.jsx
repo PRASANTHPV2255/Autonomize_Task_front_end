2e3e4cdded6884c87dca899093f42a014baf23ec
Prasanth <prasanthpv30@gmail.com>
2024-07-23 15:04:00 +0530

    first commit

---
+++ src/components/header/index.jsx
@@ -0,0 +1,12 @@
+
+import './style.css'
+function index() {
+  return (
+      <header className="header__container">
+        <h3>Autonomize AI Machine Test</h3>
+        <button>Home</button>
+      </header>
+  )
+}
+
+export default index
